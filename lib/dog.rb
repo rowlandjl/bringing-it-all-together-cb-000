5960f87299fb20994b57c98655c711e1700c5338
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 01:33:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,10 @@ class Dog
   attr_accessor :name, :breed 
   attr_reader :id 
   
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+  def initialize(id: nil, :name, :breed)
+    @id = id 
+    @name = name 
+    @breed = breed 
   end 
   
   def self.create_table 
