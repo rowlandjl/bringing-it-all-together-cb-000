76bb98213879cad9c9be733cc6609a22155f9b9b
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 01:07:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,8 +52,8 @@ class Dog
   def self.new_from_db(row) 
     new_dog = self.new
     new_dog.id = row[:id]
-    new_dog.name = row[1]
-    new_dog.breed = row[2]
+    new_dog.name = row[:name]
+    new_dog.breed = row[:breed]
     new_dog
   end 
   
