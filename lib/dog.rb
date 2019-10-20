a5b2b62c1743ef308f29fc49f410052b58ffbec8
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 01:54:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -78,7 +78,7 @@ class Dog
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else 
-      dog = Dog.create
+      dog = Dog.create(name: name, breed: breed)
     end 
   end 
   
