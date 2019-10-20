e420e657d887b087dd16123ab06b84b25eeba066
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 00:37:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,6 +9,13 @@ class Dog
   end 
   
   def self.create_table 
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
   end 
   
 end 
