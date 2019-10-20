0c0d80520b999ba3234b129e7f05e32022b80dac
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 01:52:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,6 +77,7 @@ class Dog
     if !dog.empty?
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    end 
   end 
   
   def self.find_by_name(name)
