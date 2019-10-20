bd0c24e85b0dbbc1d30d8808d53c2c237fad34d5
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-20 02:03:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,6 +40,7 @@ class Dog
       dog = DB[:conn].execute(sql, self.name, self.breed)
       
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self 
     end 
   end 
   
